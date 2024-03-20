e2b4ef9d925e9a6203b5f608ea31ee28dfe1f0ef
furryCthulhu <necuratt@yahoo.es>
2024-03-20 11:34:06 +0100

    fizzbuzznumber API respons ok

---
+++ FizzBuzz/Controllers/FizzBuzzNumberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzz.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FizzBuzzNumberController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public ActionResult Get(short number) 
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzNumberControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using FizzBuzzTests.Helpers;
+using System.Net;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzNumberControllerTests
+    {
+        private readonly CustomApplicationFactory _webApp;
+
+        public FizzBuzzNumberControllerTests()
+        {
+            _webApp ??= new CustomApplicationFactory();
+        }
+
+        [Fact]
+        public async Task GetFizzBuzzNumber_Should_Api_Return_OK_When_Number_is_1()
+        {
+            var number = 1;
+
+            var result = await _webApp.GetHttpClient().GetAsync($"fizzbuzznumber/{number}");
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
